2fe502e39d3214f80162c0490fb00091025f031a
Alexsandr Pavlov <buterfeed@gmail.com>
2025-04-10 17:15:06 +0300

    time

--- src/components/TicketsList/Ways.jsx
+++ src/components/TicketsList/Ways.jsx
@@ -3,9 +3,13 @@ import React from 'react';
 import WaysStyles from './MainTicket.module.scss';
 import {getCurrentTime} from '../../assets/getCurrentTime';
 
-export const Ways = ({origin, destination, duration, stops}) => {
+export const Ways = ({origin, destination, duration, stops, date}) => {
   const countStops = stops.length;
   let id = [...stops].length;
+  const time = new Date(date);
+  const hours = time.getUTCHours().toString().padStart(2, '0');
+  const minutes = time.getUTCMinutes().toString().padStart(2, '0');
+  const formattedTime = `${hours}:${minutes} — ${(parseInt(hours) + 1).toString().padStart(2, '0')}:${minutes}`;
   const wordForStops = countStops === 1 ? `${countStops} пересадка` : `${countStops} пересадки`;
   const stopsSpan = stops.map((item) => {
     return <span key={id--}>{item}</span>;
@@ -17,7 +21,7 @@ export const Ways = ({origin, destination, duration, stops}) => {
         <span>
           {origin} – {destination}
         </span>
-        <span> 10:45 – 08:00 </span>
+        <span> {formattedTime} </span>
       </div>
       <div>
         <span>В пути</span>
